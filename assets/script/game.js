de6398b15152f8d3a7aed35ccd045a30b4e5dafe
杨帆 <yangfan3@mgtv.com>
2022-07-01 11:40:09 +0800

    init

---
+++ assets/script/game.js
@@ -0,0 +1,40 @@
+cc.Class({
+  extends: cc.Component,
+
+  properties: {
+    mapNode: cc.Node,
+  },
+
+  onLoad() {
+    cc.director.getPhysicsManager().enabled = true;
+    this.initMapNode(this.mapNode)
+  },
+
+  start() {},
+  initMapNode(mapNode) {
+    let tiledMap = mapNode.getComponent(cc.TiledMap);
+    let tiledSize = tiledMap.getTileSize();
+    let layer = tiledMap.getLayer("wall");
+    let layerSize = layer.getLayerSize();
+
+    debugger
+
+    for (let i = 0; i < layerSize.width; i++) {
+      for (let j = 0; j < layerSize.height; j++) {
+        let tiled = layer.getTiledTileAt(i, j, true);
+        if (tiled.gid != 0) {
+          tiled.node.group = "wall";
+
+          let body = tiled.node.addComponent(cc.RigidBody);
+          body.type = cc.RigidBodyType.Static;
+          let collider = tiled.node.addComponent(cc.PhysicsBoxCollider);
+          collider.offset = cc.v2(tiledSize.width / 2, tiledSize.height / 2);
+          collider.size = tiledSize;
+          collider.apply();
+        }
+      }
+    }
+  },
+
+  // update (dt) {},
+});
