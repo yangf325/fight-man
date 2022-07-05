ce2096ded4f50585846cce2b82d4fd8f4f188a46
杨帆 <yangfan3@mgtv.com>
2022-07-05 14:41:46 +0800

    代码简化

--- assets/script/hero.js
+++ assets/script/hero.js
@@ -13,10 +13,10 @@ cc.Class({
     this._speed = 200;
     this.sp = cc.v2(0, 0);
     this.combo = 0;
-    this.anima = "idle";
-    this.aniToSet = "idle";
+    this.rb = this.node.getComponent(cc.RigidBody);
     this.heroState = State.stand;
     this.heroAni = this.node.getComponent(cc.Animation);
+    this.setAni("idle");
     this.heroAni.on("finished", this.onAnimaFinished, this);
 
     cc.systemEvent.on("keydown", this.onKeyDown, this);
@@ -36,13 +36,13 @@ cc.Class({
     this.heroAni.off("Finished", this.onAnimaFinished, this);
   },
 
-  setAni() {
-    if (this.anima == this.aniToSet) {
+  setAni(anima) {
+    if (this.anima == anima) {
       return;
     }
 
-    this.anima = this.aniToSet;
-    this.heroAni.play(this.aniToSet);
+    this.anima = anima;
+    this.heroAni.play(anima);
   },
   onAnimaFinished(event, data) {
     if (data.name.indexOf("attack") > -1) {
@@ -59,28 +59,36 @@ cc.Class({
   attack() {
     this.sp.x = 0;
     if (Input[cc.macro.KEY.j]) {
-      this.aniToSet = `attack${this.combo + 1}`;
+      this.setAni(`attack${this.combo + 1}`);
     }
   },
   move(scaleX) {
     // 判断行走方向
+    this.lv = this.rb.linearVelocity;
     if (Input[cc.macro.KEY.a] || Input[cc.macro.KEY.left]) {
       this.node.scaleX = -scaleX;
-      this.aniToSet = "run";
+      this.setAni("run");
       this.sp.x = -1;
     } else if (Input[cc.macro.KEY.d] || Input[cc.macro.KEY.right]) {
       this.node.scaleX = scaleX;
-      this.aniToSet = "run";
+      this.setAni("run");
       this.sp.x = 1;
     } else {
       this.sp.x = 0;
-      this.aniToSet = "idle";
+      this.setAni("idle");
     }
+
+    // 移动
+    if (this.sp.x) {
+      this.lv.x = this.sp.x * this._speed;
+    } else {
+      this.lv.x = 0;
+    }
+    this.rb.linearVelocity = this.lv;
   },
 
   update(dt) {
     let scaleX = Math.abs(this.node.scaleX);
-    this.lv = this.node.getComponent(cc.RigidBody).linearVelocity;
 
     // 处理状态
     switch (this.heroState) {
@@ -98,14 +106,5 @@ cc.Class({
     } else if (this.heroState == State.stand) {
       this.move(scaleX);
     }
-
-    // 移动
-    if (this.sp.x) {
-      this.lv.x = this.sp.x * this._speed;
-    } else {
-      this.lv.x = 0;
-    }
-    this.setAni(this.aniToSet);
-    this.node.getComponent(cc.RigidBody).linearVelocity = this.lv;
   },
 });
